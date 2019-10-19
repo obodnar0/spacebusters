21c590429d3a20506a08ba3de955fc3376f2cba3
Oleksandr Bodnar <bodnaralex00@gmail.com>
2019-10-19 20:12:26 +0300

    added new planets and temperature slider

---
+++ app/Assets/Scripts/EarthAtmosphere.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class EarthAtmosphere : MonoBehaviour
+{
+    public float speed;
+    private static Renderer _rend;
+    private static float transparency;
+
+    void Start()
+    {
+        _rend = GetComponent<Renderer>();
+        _rend.enabled = true;
+        transparency = _rend.material.color.a;
+    }
+
+    public static void ChangeTransparency(float trans)
+    {
+        transparency = trans;
+    }
+
+    public void Update()
+    {
+        _rend.material.color = new Color(_rend.material.color.r, _rend.material.color.g, _rend.material.color.b, transparency);
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
--- app/Assets/Scripts/PlanetConfigurator.cs
+++ app/Assets/Scripts/PlanetConfigurator.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
 
 public class PlanetConfigurator : MonoBehaviour
 {
     public Material[] _materials;
     private Renderer _rend;
-    
+
+    public static double Temperature = 10;
+
+    public static void SetTemperature(double temp)
+    {
+        Temperature = temp;
+    }
 
     void Start()
     {
         _rend = GetComponent<Renderer>();
         _rend.enabled = true;
-        _rend.sharedMaterial = _materials[0];
+        _materials = _rend.sharedMaterials;
     }
 
     void OnMouseDown()
@@ -26,7 +31,31 @@ public class PlanetConfigurator : MonoBehaviour
         CameraRotator.IsPressed = false;
     }
 
+    void UpdateMaterial(string name)
+    {
+       
+    }
+
     void Update()
     {
+        if (Temperature < -80)
+        {
+            UpdateMaterial("FrozenPlanetTexture");
+        }
+
+        if (Temperature > -80 && Temperature < 0)
+        {
+            UpdateMaterial("PlanetTexture");
+        }
+
+        if (Temperature > 0 && Temperature < 120)
+        {
+            UpdateMaterial("Earth");
+        }
+
+        if (Temperature > 120)
+        {
+            UpdateMaterial("HotPlanet");
+        }
     }
 }
