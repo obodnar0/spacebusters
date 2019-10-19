186af462cee760e348ce32e8329a27cea6d5f6a0
Oleksandr Bodnar <bodnaralex00@gmail.com>
2019-10-19 17:35:31 +0300

    added sun and camera rotation

---
+++ app/Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraRotator : MonoBehaviour
+{
+    //public float speed;
+    //public float scale = 1;
+    public static bool IsPressed = false;
+    private static float PrevX;
+    private static float PrevY;
+
+    public static void ResetPrev()
+    {
+        PrevX = Input.mousePosition.x; 
+        PrevY = Input.mousePosition.y;
+    }
+
+
+    void Update()
+    {
+        if (IsPressed)
+        {
+            transform.Rotate((PrevY - Input.mousePosition.y % 360), -(PrevX - Input.mousePosition.x % 360), 0);
+            PrevX = Input.mousePosition.x;
+            PrevY = Input.mousePosition.y;
+        }
+    }
+}
---
+++ app/Assets/Scripts/PlanetConfigurator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetConfigurator : MonoBehaviour
+{
+    public Material[] _materials;
+    private Renderer _rend;
+    
+
+    void Start()
+    {
+        _rend = GetComponent<Renderer>();
+        _rend.enabled = true;
+        _rend.sharedMaterial = _materials[0];
+    }
+
+    void OnMouseDown()
+    {
+        CameraRotator.ResetPrev();
+        CameraRotator.IsPressed = true;
+    }
+
+    void OnMouseUp()
+    {
+        CameraRotator.IsPressed = false;
+    }
+
+    void Update()
+    {
+    }
+}
