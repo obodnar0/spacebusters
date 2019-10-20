97cc85965017188a941c9a366ae7c6309c6fcc78
Vladyslav <vladyslav.ishchuk@gmail.com>
2019-10-20 13:10:30 +0300

    additional

---
+++ app/Assets/Scripts/SateliteSlider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SateliteSlider : MonoBehaviour
+{
+    private Slider SateliteSliderInstance;
+    private int prevVal = -1;
+
+    void Start()
+    {
+        SateliteSliderInstance = GameObject.Find("SatelitSlider").GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        var val = (int)SateliteSliderInstance.value;
+        if (prevVal == val)
+            return;
+
+        List<GameObject> Satelites = new List<GameObject>();
+        Satelites.Add(ObjectsStorage.Ints.Satel1);
+        Satelites.Add(ObjectsStorage.Ints.Satel2);
+        Satelites.Add(ObjectsStorage.Ints.Satel3);
+        Satelites.Add(ObjectsStorage.Ints.Satel4);
+
+        for (int i = 0; i < val; i++)
+        {
+            Satelites[i].SetActive(true);
+        }
+
+        for (int i = val; i < Satelites.Count; i++)
+        {
+            Satelites[i].SetActive(false);
+        }
+    }
+}
