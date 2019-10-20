d8137669dd8f311bc84431ea04ec66d9c1769fbc
Oleksandr Bodnar <bodnaralex00@gmail.com>
2019-10-20 12:01:42 +0300

    Added missing scripts

---
+++ app/Assets/Scripts/StarRadiusScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarRadiusScript : MonoBehaviour
+{
+    private Slider RadSlider;
+
+    void Start()
+    {
+        RadSlider = GameObject.Find("SRSlider").GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        var gs = RadSlider.value / 1000;
+        ObjectsStorage.Ints.Star.transform.localScale = new Vector3(-gs,-gs,-gs);
+    }
+}
